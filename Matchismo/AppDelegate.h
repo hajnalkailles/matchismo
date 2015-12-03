5ba080e6f08238c14d0e38bc5fd9cb9e2821f56c
Hegyi Hajnalka <hajhegyi@hegyis-mbp.misys.global.ad>
2015-12-03 12:10:19 +0100

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Hegyi Hajnalka on 03/12/15.
+//  Copyright © 2015 Hegyi Hajnalka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Hegyi Hajnalka on 03/12/15.
+//  Copyright © 2015 Hegyi Hajnalka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
